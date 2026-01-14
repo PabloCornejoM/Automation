dba06dea348b8e72e33dc072d255bde65123ae83
PabloCornejoM <pablo.cornejo01@gmail.com>
2026-01-14 17:25:24 -0300

    Update .gitignore, environment.yml, test.py, and add new Cron notebook

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
+import time
+
 def write_file():
 	with open('test.txt', 'a+') as f:
-		f.write('Writing from Cron\n')
+		f.write('Writing from Cron ' + time.strftime("%Y-%m-%d %H:%M:%S") + '\n')
 		
 if __name__ == '__main__':
 	write_file()
